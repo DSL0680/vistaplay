ec807de12b655841ade037e0c905aec6b819593a
DSL0680 <ekthf988172@gmail.com>
2024-10-05 23:54:45 +0900

    ContentListComponent 연결

--- src/pages/contents/ContentListPage.tsx
+++ src/pages/contents/ContentListPage.tsx
@@ -1,8 +1,10 @@
+import ContentListComponent from "../../components/contents/ContentListComponent.tsx";
 
 function ContentListPage() {
     return (
         <div>
             <h1>Content List Page</h1>
+            <ContentListComponent/>
         </div>
     );
 }
