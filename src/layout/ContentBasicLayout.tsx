cb7be381025475dc930bd01957322296c39109bc
DSL0680 <ekthf988172@gmail.com>
2024-10-05 23:52:39 +0900

    오류 수정

--- src/layout/ContentBasicLayout.tsx
+++ src/layout/ContentBasicLayout.tsx
@@ -18,10 +18,9 @@ function ContentBasicLayout({children}: {children: React.ReactNode}): ReactEleme
                         <button type="button"
                                 className="-m-2.5 inline-flex items-center justify-center rounded-md p-2.5 text-gray-700">
                             <span className="sr-only">Open main menu</span>
-                            <svg className="h-6 w-6" fill="none" viewBox="0 0 24 24" stroke-width="1.5"
+                            <svg className="h-6 w-6" fill="none" viewBox="0 0 24 24" strokeWidth="1.5"
                                  stroke="currentColor" aria-hidden="true">
-                                <path stroke-linecap="round" stroke-linejoin="round"
-                                      d="M3.75 6.75h16.5M3.75 12h16.5m-16.5 5.25h16.5"/>
+                                <path strokeLinecap="round"/>
                             </svg>
                         </button>
                     </div>
@@ -35,7 +34,7 @@ function ContentBasicLayout({children}: {children: React.ReactNode}): ReactEleme
                             </button>
                         </div>
 
-                        <a href="#" className="text-sm font-semibold leading-6 text-gray-900">Features</a>
+                        <a href="/admin/add" className="text-sm font-semibold leading-6 text-gray-900">Features</a>
                         <a href="#" className="text-sm font-semibold leading-6 text-gray-900">Marketplace</a>
                         <a href="#" className="text-sm font-semibold leading-6 text-gray-900">Company</a>
                     </div>
@@ -76,9 +75,9 @@ function ContentBasicLayout({children}: {children: React.ReactNode}): ReactEleme
                             </a>
                             <button type="button" className="-m-2.5 rounded-md p-2.5 text-gray-700">
                                 <span className="sr-only">Close menu</span>
-                                <svg className="h-6 w-6" fill="none" viewBox="0 0 24 24" stroke-width="1.5"
+                                <svg className="h-6 w-6" fill="none" viewBox="0 0 24 24" strokeWidth="1.5"
                                      stroke="currentColor" aria-hidden="true">
-                                    <path stroke-linecap="round" stroke-linejoin="round" d="M6 18L18 6M6 6l12 12"/>
+                                    <path strokeLinecap="round"/>
                                 </svg>
                             </button>
                         </div>
@@ -92,9 +91,9 @@ function ContentBasicLayout({children}: {children: React.ReactNode}): ReactEleme
                                             Product
                                             <svg className="h-5 w-5 flex-none" viewBox="0 0 20 20" fill="currentColor"
                                                  aria-hidden="true">
-                                                <path fill-rule="evenodd"
+                                                <path fillRule="evenodd"
                                                       d="M5.23 7.21a.75.75 0 011.06.02L10 11.168l3.71-3.938a.75.75 0 111.08 1.04l-4.25 4.5a.75.75 0 01-1.08 0l-4.25-4.5a.75.75 0 01.02-1.06z"
-                                                      clip-rule="evenodd"/>
+                                                      clipRule="evenodd"/>
                                             </svg>
                                         </button>
 
