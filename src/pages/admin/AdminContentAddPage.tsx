7c611460f15e3e1cb880ae3c04cea028abbf691a
DSL0680 <ekthf988172@gmail.com>
2024-10-05 23:50:52 +0900

    AdminContentAddComponent 연결

--- src/pages/admin/AdminContentAddPage.tsx
+++ src/pages/admin/AdminContentAddPage.tsx
@@ -1,8 +1,10 @@
+import AdminContentAddComponent from "../../components/admin/AdminContentAddComponent.tsx";
 
 function AdminContentAddPage() {
     return (
         <div>
             <h1>Admin Content Add Page</h1>
+            <AdminContentAddComponent/>
         </div>
     );
 }
