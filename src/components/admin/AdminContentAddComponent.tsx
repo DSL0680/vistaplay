4054551e5a5f7dd49876e43f40b309db00506363
DSL0680 <ekthf988172@gmail.com>
2024-10-07 11:35:04 +0900

    AdminContentAddComponent.tsx

--- src/components/admin/AdminContentAddComponent.tsx
+++ src/components/admin/AdminContentAddComponent.tsx
@@ -29,17 +29,17 @@ function AdminContentAddComponent() {
             const [type, genre] = value.split('-');
             setContent(prevContent => ({
                 ...prevContent,
-                keyword: value,
                 type,
-                genre
+                genre,
+                keyword: value
             }));
         } else if (name === 'pdesc') {
             const [ex, link] = value.split(',');
             setContent(prevContent => ({
                 ...prevContent,
-                pdesc: value,
                 ex,
-                link
+                link,
+                pdesc: value
             }));
         } else {
             setContent(prevContent => ({
@@ -51,14 +51,11 @@ function AdminContentAddComponent() {
 
     const handleFileChange = (e: ChangeEvent<HTMLInputElement>) => {
         if (e.target.files) {
-
             const filesArray = Array.from(e.target.files);
             setContent(prevContent => ({
                 ...prevContent,
                 files: [...prevContent.files, ...filesArray],
             }));
-
-            console.log("Selected files:", filesArray);
         }
     };
 
@@ -73,12 +70,6 @@ function AdminContentAddComponent() {
         content.files.forEach(file => {
             formData.append("files", file);
         });
-        console.log("Form data to be sent:", {
-            pname: content.pname,
-            pdesc: content.pdesc,
-            keyword: content.keyword,
-            files: content.files
-        });
 
         postAdd(formData)
             .then((pno: number) => {
@@ -91,7 +82,6 @@ function AdminContentAddComponent() {
             });
     };
 
-
     const closeCallback = () => {
         setResult('');
         setContent({ ...initState });
@@ -99,71 +89,87 @@ function AdminContentAddComponent() {
     };
 
     return (
-        <div className='flex flex-col space-y-4 w-96 mx-auto'>
-            {loading && <LoadingComponent />}
-            {result && <ResultModal msg={result} callback={closeCallback} />}
+        <div className="flex justify-center items-center min-h-screen bg-gray-100">
+            <div className="w-full p-8 bg-gray-50 rounded-lg shadow-lg">
+                {loading && <LoadingComponent />}
+                {result && <ResultModal msg={result} callback={closeCallback} />}
 
-            <div>Admin Content Add</div>
+                <div className="mb-6 text-3xl font-bold text-center text-gray-800">작품 등록</div>
 
-            <label className="text-sm font-semibold text-gray-700">제목</label>
-            <input
-                className="border border-gray-300 rounded-lg p-3 focus:outline-none focus:ring-2 focus:ring-blue-500 focus:border-transparent transition duration-300"
-                type='text'
-                name='pname'
-                value={content.pname}
-                placeholder="작품 제목"
-                onChange={handleChange}
-            />
+                <div className="grid grid-cols-2 gap-6">
+                    {/* 왼쪽 칸 */}
+                    <div>
+                        <label className="text-sm font-semibold text-gray-700">제목</label>
+                        <input
+                            className="border border-gray-300 rounded-lg p-3 w-full focus:outline-none focus:ring-2 focus:ring-blue-500 focus:border-transparent transition duration-300 shadow-sm"
+                            type='text'
+                            name='pname'
+                            value={content.pname}
+                            placeholder="작품 제목"
+                            onChange={handleChange}
+                        />
 
-            <label className="text-sm font-semibold text-gray-700">작품 설명 & 예고편 링크</label>
-            <textarea
-                className="border border-gray-300 rounded-lg p-3 focus:outline-none focus:ring-2 focus:ring-blue-500 focus:border-transparent transition duration-300"
-                name='pdesc'
-                placeholder="작품 설명 & 예고편 링크"
-                value={content.pdesc}
-                onChange={handleChange}
-            />
+                        <label className="text-sm font-semibold text-gray-700 mt-4">작품 설명 & 예고편 링크</label>
+                        <textarea
+                            className="border border-gray-300 rounded-lg p-3 w-full focus:outline-none focus:ring-2 focus:ring-blue-500 focus:border-transparent transition duration-300 shadow-sm"
+                            name='pdesc'
+                            placeholder="작품 설명 & 예고편 링크"
+                            value={content.pdesc}
+                            onChange={handleChange}
+                            rows={10} // 행 수를 늘려서 높이를 조절
+                        />
+                    </div>
 
-            <label className="text-sm font-semibold text-gray-700">타입 & 장르</label>
-            <select
-                className="border border-gray-300 rounded-lg p-3 focus:outline-none focus:ring-2 focus:ring-blue-500 focus:border-transparent transition duration-300"
-                name="keyword"
-                value={content.keyword}
-                onChange={handleChange}
-            >
-                <option value="">선택</option>
-                <option value="영화 - 액션">영화 - 액션</option>
-                <option value="영화 - 코미디">영화 - 코미디</option>
-                <option value="영화 - 공포">영화 - 공포</option>
-                <option value="영화 - 가족">영화 - 가족</option>
-                <option value="드라마 - 로맨스">드라마 - 로맨스</option>
-                <option value="드라마 - 모험">드라마 - 모험</option>
-                <option value="드라마 - 범죄">드라마 - 범죄</option>
-                <option value="예능 - 음악">예능 - 음악</option>
-                <option value="예능 - 스릴러">예능 - 스릴러</option>
-                <option value="애니메이션 - 공상과학">애니메이션 - 공상과학</option>
-                <option value="애니메이션 - 판타지">애니메이션 - 판타지</option>
-                <option value="애니메이션 - 전쟁">애니메이션 - 전쟁</option>
-                <option value="다큐멘터리 - 액션">다큐멘터리 - 액션</option>
-                <option value="다큐멘터리 - 코미디">다큐멘터리 - 코미디</option>
-                <option value="다큐멘터리 - 스릴러">다큐멘터리 - 스릴러</option>
-            </select>
+                    {/* 오른쪽 칸 */}
+                    <div>
+                        <label className="text-sm font-semibold text-gray-700">이미지 첨부</label>
+                        <input
+                            className="border border-gray-300 rounded-lg p-3 w-full focus:outline-none focus:ring-2 focus:ring-blue-500 focus:border-transparent transition duration-300 shadow-sm"
+                            type="file"
+                            accept="image/*"
+                            multiple
+                            onChange={handleFileChange}
+                        />
 
-            <label className="text-sm font-semibold text-gray-700">Upload Image</label>
-            <input
-                className="border border-gray-300 rounded-lg p-3 focus:outline-none focus:ring-2 focus:ring-blue-500 focus:border-transparent transition duration-300"
-                type="file"
-                accept="image/*"
-                onChange={handleFileChange}
-            />
+                        <label className="text-sm font-semibold text-gray-700 mt-4">장르 태그 추가</label>
+                        <div className="flex space-x-4">
+                            <select
+                                className="border border-gray-300 rounded-lg p-2 w-1/2 focus:outline-none focus:ring-2 focus:ring-blue-500 focus:border-transparent transition duration-300 shadow-sm"
+                                name="keyword"
+                                value={content.keyword}
+                                onChange={handleChange}
+                            >
+                                <option value="">선택</option>
+                                <option value="영화 - 액션">영화 - 액션</option>
+                                <option value="영화 - 코미디">영화 - 코미디</option>
+                                <option value="영화 - 공포">영화 - 공포</option>
+                                <option value="영화 - 가족">영화 - 가족</option>
+                                <option value="드라마 - 로맨스">드라마 - 로맨스</option>
+                                <option value="드라마 - 모험">드라마 - 모험</option>
+                                <option value="드라마 - 범죄">드라마 - 범죄</option>
+                                <option value="예능 - 음악">예능 - 음악</option>
+                                <option value="예능 - 스릴러">예능 - 스릴러</option>
+                                <option value="애니메이션 - 공상과학">애니메이션 - 공상과학</option>
+                                <option value="애니메이션 - 판타지">애니메이션 - 판타지</option>
+                                <option value="애니메이션 - 전쟁">애니메이션 - 전쟁</option>
+                                <option value="다큐멘터리 - 액션">다큐멘터리 - 액션</option>
+                                <option value="다큐멘터리 - 코미디">다큐멘터리 - 코미디</option>
+                                <option value="다큐멘터리 - 스릴러">다큐멘터리 - 스릴러</option>
+                            </select>
+                        </div>
+                    </div>
+                </div>
 
-            <button
-                type="submit"
-                className="bg-blue-500 text-white font-semibold py-3 px-6 rounded-lg shadow-lg hover:bg-blue-600 focus:outline-none focus:ring-4 focus:ring-blue-300 transition duration-300"
-                onClick={handleClick}
-            >
-                등록
-            </button>
+                <div className="text-center mt-6">
+                    <button
+                        type="submit"
+                        className="bg-blue-500 text-white font-semibold py-3 px-6 rounded-lg shadow-lg hover:bg-blue-600 transition duration-300 focus:outline-none focus:ring-4 focus:ring-blue-300"
+                        onClick={handleClick}
+                    >
+                        등록
+                    </button>
+                </div>
+            </div>
         </div>
     );
 }
